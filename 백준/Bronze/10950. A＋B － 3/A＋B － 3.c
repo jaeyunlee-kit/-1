28ae3cf6443e4a4f93473018b0ab6277f61690f2
jaeyunlee-kit <144886697+jaeyunlee-kit@users.noreply.github.com>
2023-09-26 19:59:44 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d", &c);
+    
+    for(int i=1; i<=c; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("%d\n",a+b);
+    }
+}
