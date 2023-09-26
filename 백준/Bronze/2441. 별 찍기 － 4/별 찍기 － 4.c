64f2aa8861b8b801638a51361dfbbdebb0dc846b
jaeyunlee-kit <144886697+jaeyunlee-kit@users.noreply.github.com>
2023-09-26 20:24:48 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    b = a;
+    for (int is = 1; is <= a; is++)
+    {
+        for (int i = a; i > b; i--)
+        {
+            printf(" ");
+        }
+
+        for (int i = 1; i <= b; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        b--;
+    }
+}
