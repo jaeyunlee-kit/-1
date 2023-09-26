25c92ddd27d3c4c1aeb657e0f95b03b39d25439b
jaeyunlee-kit <144886697+jaeyunlee-kit@users.noreply.github.com>
2023-09-26 19:53:23 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b, c;
+    scanf("%lld %lld", &a, &b);
+    c = a-b;
+    
+    if (a-b<0)
+    {
+        printf("%lld", -c);
+    }
+    
+    else
+    {
+        printf("%lld", c);
+    }
+}
