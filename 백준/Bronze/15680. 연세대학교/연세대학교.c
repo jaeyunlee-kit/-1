0ee0a90404a2b296db5672d231d2c4f6d379eb42
jaeyunlee-kit <144886697+jaeyunlee-kit@users.noreply.github.com>
2023-11-21 20:02:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+	if (N == 0)
+		printf("YONSEI");
+	else
+		printf("Leading the Way to the Future");
+}
